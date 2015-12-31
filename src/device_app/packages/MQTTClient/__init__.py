829d1b9b5abbbb963acffeb0cb48df895fbf4f0b
Jake Callery <jcallery@subvoicestudios.com>
2015-12-31 13:39:53 -0500

    subscribe and message receiving works

--- src/device_app/packages/MQTTClient/__init__.py
+++ src/device_app/packages/MQTTClient/__init__.py
@@ -19,11 +19,13 @@ class MQTTClient:
 
 	def _on_connect(self, client, object, flags, rc):
 		self._log.info("Subscriber Connection status code: " + str(rc) + " | Connection status: successful")
+		self._client.subscribe('AWSLightGrid/status')
+		self._client.subscribe('AWSLightGrid/get')
 
 	def _on_subscribe(self, client, obj, mid, granted_qos):
 		self._log.info("Subscribed: " + str(mid) + " " + str(granted_qos) + "  data:" + str(obj))
 
-	def _on_message(self, obj, msg):
+	def _on_message(self, client, userdata, msg):
 		self._log.debug("Received message from topic: " + msg.topic + " | QoS: " + str(msg.qos) + " | Data Received: " + str(msg.payload))
 
 	def connect(self, host=None, port=None):
@@ -32,6 +34,7 @@ class MQTTClient:
 
 		#TODO: deal with message pump a different way
 		self._client.loop_forever()
+		#self._client.loop_start()
 		self._log.debug('After Loop Forever')
 
 
