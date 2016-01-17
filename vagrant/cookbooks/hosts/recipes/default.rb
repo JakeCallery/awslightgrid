a716481af3cb0a7159fa738e7ba2b87dd26bc6a4
Jake Callery <jcallery@subvoicestudios.com>
2016-01-17 15:35:11 -0500

    updated to use jakecallery.com

--- vagrant/cookbooks/hosts/recipes/default.rb
+++ vagrant/cookbooks/hosts/recipes/default.rb
@@ -1,15 +1,15 @@
 execute "Change hostname" do
-	command "echo 'awslightgrid.ngrok.io' > /etc/hostname"
+	command "echo 'awslightgrid.jakecallery.com' > /etc/hostname"
 end
 
 execute "run hostname" do
-	command "hostname awslightgrid.ngrok.io"
+	command "hostname awslightgrid.jakecallery.com"
 end
 
 ruby_block "insert_line" do
   block do
     file = Chef::Util::FileEdit.new("/etc/hosts")
-	  file.insert_line_if_no_match("127.0.0.1 awslightgrid.ngrok.io", "127.0.0.1 awslightgrid.ngrok.io")
+	  file.insert_line_if_no_match("127.0.0.1 awslightgrid.jakecallery.com", "127.0.0.1 awslightgrid.jakecallery.com")
     file.write_file
   end
 end
