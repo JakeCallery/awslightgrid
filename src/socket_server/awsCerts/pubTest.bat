d892fb58c74935fdcaf30a768e1965f858108e08
Jake <jake@subvoicestudios.com>
2023-09-28 16:04:24 -0500

    rolled certs

--- src/socket_server/awsCerts/pubTest.bat
+++
@@ -1,1 +0,0 @@
-"c:\Program Files (x86)\mosquitto\mosquitto_pub.exe" --cafile root-CA.crt --cert certificate.pem.crt --key private.pem.key -h "A330MM7RATC5K1.iot.us-east-1.amazonaws.com" -p 8883 -q 1 -d -t topic_1 -i clientid2 -m "Hi Data"
